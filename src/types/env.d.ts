a446545e399857fc7f4104f1d47ba71d7303fb00
Santiago Carrasco <jsanticc@hotmail.com>
2021-07-19 13:56:45 -0700

    Add default PORT if its not added on env file

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
-    PORT?: string;
+    PORT: string | 4000;
     SECRET_SESSION: string;
   }
 }
